a385cc0459d625a426ef89310f9b5c5b7775a7a3
Shaozaki <fatihdaffauldzaki@gmail.com>
2025-01-02 16:17:54 +0700

    linked list

---
+++ Linked List praktik/src/linkedlistEX.java
@@ -0,0 +1,21 @@
+import java.util.LinkedList;
+public class linkedlistEX {
+    public static void main(String[] args) {
+        // Membuat instance atau objek daarai linkedList
+        LinkedList <String> buah = new LinkedList<>();
+        
+        buah.add("Jeruk");
+        buah.add("Jambu");
+        buah.add("Apel");
+        buah.add("Melon");
+        buah.add("Semangka");
+        buah.add("Semangka");
+        buah.add("Nanas");
+        buah.add("Sirsak");
+
+        System.out.println("Saya Makan Buah"+buah);
+
+        System.out.println("Di rumah saya setiap hari menydiakan "+buah.size()+" Macam buah-buahan");
+        
+    }
+}
