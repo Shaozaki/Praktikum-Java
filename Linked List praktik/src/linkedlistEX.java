a11e8943ab59dde9d24a502a2c7a3d6f708b8696
Shaozaki <fatihdaffauldzaki@gmail.com>
2025-01-02 20:57:24 +0700

    tambahin item ke indexs bawah dan atas

--- Linked List praktik/src/linkedlistEX.java
+++ Linked List praktik/src/linkedlistEX.java
@@ -14,6 +14,9 @@ public class linkedlistEX {
         buah.add("Nanas");
         buah.add("Sirsak");
         //Mencetak data
+        
+        buah.addFirst ("Paling atas");//Menambahkan data ke indexs paling atas
+        buah.addLast("Paling Bawah");//Menambahkan data ke indexs paling bawah
         System.out.println("Saya Makan Buah "+buah);
         //Menampilkan jumlah item 
         System.out.println("Di rumah saya setiap hari menyediakan "+buah.size()+" Macam buah-buahan");
