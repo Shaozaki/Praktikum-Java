f2d132bb5b7416cadc6668c057fd7d794b2d89ef
Shaozaki <fatihdaffauldzaki@gmail.com>
2025-01-03 21:49:47 +0700

    g

---
+++ Linked List praktik/src/linkedlist5.java
@@ -0,0 +1,22 @@
+import java.util.LinkedList;
+public class linkedlist5 {
+    public static void main(String[] args) {
+        LinkedList<String> barang = new LinkedList<>();
+        
+        barang.add("Laptop");
+        barang.add("Komputer");
+        barang.add("Radio");
+
+        if(barang.isEmpty()){
+            System.out.println("Data Kosong");
+        }else{
+            System.out.println("Data Penuh");
+        }
+
+        if(barang.contains("Laptop")){
+            System.out.println("Barang ditemukan");
+        }else{
+            System.out.println("Barang tdak ditemukan");
+        }
+    }
+}
