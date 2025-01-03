724b2b8cb8e779cd4c550539e82d748e8a79598a
Shaozaki <fatihdaffauldzaki@gmail.com>
2025-01-03 21:13:10 +0700

    EX4

---
+++ Linked List praktik/src/linkedlistEX4.java
@@ -0,0 +1,22 @@
+import java.util.LinkedList;
+
+public class linkedlistEX4 {
+    public static void main(String[] args) {
+        // Membuat instance atau objek daarai linkedList
+        LinkedList <String> buah = new LinkedList<>();
+        
+        //Menambahkan Data Pada Objek
+        buah.add("Jeruk");
+        buah.add("Jambu");
+        buah.add("Melon");
+        buah.add("Semangka");
+
+       
+        buah.removeFirst();
+        buah.removeLast();
+        buah.remove(1);
+
+        System.out.println(buah);
+    }
+}
+
